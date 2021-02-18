93327f45a9050bc10390ae08ebc16d3b5f4d5041
João Lopes <joaomarcoslopes@id.uff.br>
2021-02-18 19:54:46 -0300

    chore: spring init project

---
+++ src/main/kotlin/app/civa/vaccination/VaccinationApplication.kt
@@ -0,0 +1,11 @@
+package app.civa.vaccination
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VaccinationApplication
+
+fun main(args: Array<String>) {
+	runApplication<VaccinationApplication>(*args)
+}
---
+++ src/test/kotlin/app/civa/vaccination/VaccinationApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.civa.vaccination
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VaccinationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
