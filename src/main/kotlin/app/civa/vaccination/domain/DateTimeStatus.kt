d95be0db90c451c2e49e8f7d56d82ad2ebeea8ef
João Lopes <joaomarcoslopes@id.uff.br>
2021-02-22 22:53:04 -0300

    style: fix NewLineAtEndOfFile rule violation

--- src/main/kotlin/app/civa/vaccination/domain/DateTimeStatus.kt
+++ src/main/kotlin/app/civa/vaccination/domain/DateTimeStatus.kt
@@ -5,5 +5,4 @@ enum class DateTimeStatus(val message: String) {
     BEFORE("Provided application is before today"),
     SAME("Provided application already added"),
     INTERVAL("Provided application cannot be added today");
-
 }
