47947db368503c6a512f08ed0b7847c08e255fef
João Lopes <joaomarcos467@gmail.com>
2021-04-24 02:45:54 -0300

    test(inventory): add vaccine to inventory behavior

--- src/test/kotlin/app/civa/vaccination/domain/InventoryTest.kt
+++ src/test/kotlin/app/civa/vaccination/domain/InventoryTest.kt
@@ -1,9 +1,12 @@
 package app.civa.vaccination.domain
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.maps.shouldBeEmpty
 import io.kotest.matchers.maps.shouldNotBeEmpty
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.mockk.every
@@ -14,7 +17,7 @@ import kotlin.random.Random
 
 class InventoryTest : BehaviorSpec({
     given("an empty inventory") {
-        `when`("a vaccine and a positive quantity is provided") {
+        `when`("a valid vaccine and a positive quantity is provided") {
             then("vaccine control should be added to the inventory") {
                 val inventory = Inventory()
 
@@ -26,7 +29,7 @@ class InventoryTest : BehaviorSpec({
                 }
 
                 shouldNotThrowAny {
-                    inventory.add(vaccineMock, Random.nextInt(1, 100))
+                    inventory.add(vaccineMock, quantity = Random.nextInt(1, 100))
                 }
 
                 inventory.shouldNotBeNull().shouldNotBeEmpty()
@@ -39,7 +42,7 @@ class InventoryTest : BehaviorSpec({
                 }
             }
         }
-        `when`("a vaccine is provided") {
+        `when`("a valid vaccine is provided") {
             then("one vaccine control should be added to the inventory") {
                 val inventory = Inventory()
 
@@ -62,6 +65,39 @@ class InventoryTest : BehaviorSpec({
                     vaccineMock.mustBeValid()
                     vaccineMock pairNameWith ofType<VaccineControl>()
                 }
+            }
+        }
+        `when`("a valid vaccine and a negative quantity is provided") {
+            then("no vaccine control should be added to the inventory") {
+                val inventory = Inventory()
+
+                val vaccineMock = mockk<Vaccine>()
+
+                shouldThrowExactly<IllegalQuantityException> {
+                    inventory.add(vaccineMock, quantity = -1)
+                }
+
+                inventory.shouldNotBeNull().shouldBeEmpty()
+                inventory.contains("Vaccine Test").shouldBeFalse()
+            }
+        }
+        `when`("an invalid vaccine is provided") {
+            then("no vaccine control should be added to the inventory") {
+                val inventory = Inventory()
+
+                val vaccineMock = mockk<Vaccine>()
+                every {
+                    vaccineMock.mustBeValid()
+                } throws ExpiredVaccineException("Expired Vaccine Test", "E", "A")
+
+                shouldThrowExactly<ExpiredVaccineException> {
+                    inventory.add(vaccineMock)
+                }
+
+                inventory.shouldNotBeNull().shouldBeEmpty()
+                inventory.contains("Vaccine Test").shouldBeFalse()
+
+                verify { vaccineMock.mustBeValid() }
             }
         }
     }
